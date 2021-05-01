e0171bd0df130a90dfcf5b1b090e618070f88474
Nícalo Ribeiro <nicalo63@gmail.com>
2021-05-01 19:34:55 -0300

    Adicao do componente Header

---
+++ front/src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Styles.css';
+
+
+function Header(props) {
+    return (
+        <div className="header">
+            <div>
+                <span className="encare">Encare</span>
+            </div>
+            <div>
+                {props.children}
+            </div>
+        </div>
+    )
+}
+
+export default Header;
