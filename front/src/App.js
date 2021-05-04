d986665c301ce7e0ebf284c5075aa68be5c484b8
Gustavo-Nogueira <gustavonr.13@gmail.com>
2021-05-03 23:10:08 -0300

    Adicao da integracao entre as paginas

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
-import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import Profile from "./pages/Profile";
-import Feed from "./pages/Feed";
+import Home from "./pages/Home/Home";
+import CadastroEstabelecimento from "./pages/CadastroEstabelecimento/CadastroEstabelecimento";
+
+import "./App.css";
 
 function App() {
-  // return <Profile />;
-  return <Feed />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <Home />} />
+        <Route exact path="/perfil" render={() => <Profile />} />
+        <Route
+          exact
+          path="/cadastro"
+          render={() => <CadastroEstabelecimento />}
+        />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
